56b1cb64cd36ac894c0dc1ff2345291f4d51d316
S Jonathan Harrison <121378843+jonofficial@users.noreply.github.com>
2024-02-01 20:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (20.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,7 +7,7 @@ public:
         int t1=0;
         int t2=1;
         int next=t1+t2;
-        for(int i=3;i<=n;i++){
+        for(int i=0;i<=n-3;i++){
             t1=t2;
             t2=next;
             next=t1+t2;
